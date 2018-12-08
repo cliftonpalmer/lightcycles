d4f7def595aa8528fe0ceec8de0083fede36389d
Clifton James Palmer <clifton.palmer@gmail.com>
2018-12-08 17:19:06 -0600

    Updated right angle message

--- scene.lua
+++ scene.lua
@@ -87,7 +87,7 @@ function doLinesIntersect(x1,y1, x2,y2, x3,y3, x4,y4)
                 y4 <= y1 and y1 <= y3
             )
     else
-        print('foo')
+        print('You should never see this message')
     end
 
     if intersect then
